e495208e709cb8ab2343d1eb98aaa82ce35e20fe
yangkemin <iherewaitfor@163.com>
2023-08-18 22:11:21 +0800

    update DivideAndConquer/QuickSort/main.cpp

--- DivideAndConquer/QuickSort/main.cpp
+++ DivideAndConquer/QuickSort/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 class QuickSort {
 public:
-    static void quickSort(vector<int>& arr, int L, int R) {
+    void quickSort(vector<int>& arr, int L, int R) {
         if (L >= R) {
             //递归结束条件，只剩一个元素了，就是有序的。
             return;
@@ -17,29 +17,29 @@ public:
         //子数组分解后已是有序，无需合并操作。
     }
     //分解
-    static int partition(vector<int>& arr, int L, int R) {
+    int partition(vector<int>& arr, int L, int R) {
         int x = arr[R]; //主元素
         int i = L - 1;
         for (int j = L; j < R; j++) {
             if (arr[j] < x) {
                 i = i + 1;
-                //exchange arr[i] and arr[j]
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                swap(arr, i, j);//exchange arr[i] and arr[j]
             }
-        }
-        // exchange arr[i+1] width arr[R]
-        int t = arr[i + 1];
-        arr[i + 1] = arr[R];
-        arr[R] = t;
+        }        
+        swap(arr, i + 1, R);// exchange arr[i+1] width arr[R]
         return i + 1;
+    }
+    void swap(vector<int>& arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 };
 int main()
 {
     vector<int> nums = { 5,4,3,5,683,86,3 };
-    QuickSort::quickSort(nums, 0, nums.size() - 1);
+    QuickSort quickSort;
+    quickSort.quickSort(nums, 0, nums.size() - 1);
     for (int i = 0; i < nums.size(); i++) {
         cout << nums[i] << " ";
     }
